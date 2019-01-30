d32a86959ffa79cf8112c610a3b5ad48f8620f5c
GuMMaN <trygve@igesund.com>
2019-01-30 19:35:43 +0100

    hele sulamitten

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MovieList from './MovieList';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello!</h1>
+        <MovieList />
       </div>
     );
   }
